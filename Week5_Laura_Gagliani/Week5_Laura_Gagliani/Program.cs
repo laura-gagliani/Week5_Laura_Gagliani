6cf2a4aeebebb4b8afb9ee69cde3b836ad45c4cd
Laura Gagliani <gaglianilaura@gmail.com>
2021-11-05 09:19:07 +0100

    Aggiungere i file di progetto.

---
+++ Week5_Laura_Gagliani/Week5_Laura_Gagliani/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week5_Laura_Gagliani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
